6e63e5fc150eda97ba34d9ca2816e256e2caa2f7
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-20 18:34:50 +0530

    unwanted folder removed

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '77209675ed5768e2b5de21dbcb8c0986',
-  baseUrl: 'https://api.themoviedb.org/3',
-  images: 'http://images.tmdb.org/t/p',
-};
