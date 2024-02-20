129e63d7f35c09de7f30ce7602b39b46a619bd35
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-20 15:38:32 +0530

    env prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '77209675ed5768e2b5de21dbcb8c0986',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://images.tmdb.org/t/p',
 };
